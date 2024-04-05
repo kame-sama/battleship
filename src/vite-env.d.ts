00bbf0916ad1fe70f344bfc112515b8ca83097cb
kame-sama <ad.bodeev@gmail.com>
2024-04-05 14:17:55 +0500

    Add types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="vite/client" />
+
+type AttackResult = 'hit' | 'miss' | `${ShipName} sunk`;
+
+type Board = Array<Array<AttackResult | ShipName | ''>>;
+
+type BoardName = 'player' | 'enemy';
+
+type CoordPoint = [number, number];
+
+type CoordPointKey = `[${CoordPoint}]`;
+
+type Direction = 'up' | 'right' | 'down' | 'left';
+
+type ShipName =
+  | 'carrier'
+  | 'battleship'
+  | 'cruiser'
+  | 'submarine'
+  | 'destroyer';
+
+type Ships = {
+  [key in ShipName]: Ship;
+};
